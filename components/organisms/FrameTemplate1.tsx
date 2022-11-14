e9cb0a5f95ca257dc1c86e9541eb08229349c445
mikoloism <mikoloism@gmail.com>
2022-11-14 10:37:44 +0330

    perf(organism): extract `frame template - 1` to organisms directory;

---
+++ components/organisms/FrameTemplate1.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import style from 'styles/frame-template-1.module.scss';
+import container from 'styles/home.module.scss';
+
+export function FrameTemplate1() {
+    return (
+        <div
+            className={classNames(
+                style.door_wrapper,
+                container.template__door
+            )}>
+            <div className={style.door}>
+                <div className={style.door__top}></div>
+                <div className={style.door__left}></div>
+                <div className={style.door__right}></div>
+                <div className={style.door__bottom}></div>
+                <div className={style.door__middle}></div>
+            </div>
+        </div>
+    );
+}
