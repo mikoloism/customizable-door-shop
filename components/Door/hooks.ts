5c7107a844a37e46d6cb9da16418685f2efebdcc
mikoloism <mikoloism@gmail.com>
2022-11-19 16:46:18 +0330

    feat(hooks): update `Door/hooks.ts`;
    support `percentage` and `pixel` in `withPositionProperty`; support `two coordinate` `withPositionProperty` (renamed to `withPositionVaraible`) to work with variable instead css properties;

--- components/Door/hooks.ts
+++ components/Door/hooks.ts
@@ -29,4 +29,38 @@ function computePositionProperty(
     return {};
 }
 
+const TWO_COORDINATION_PROPERTIES: TopAndLeft[] = ['top', 'left'];
+export function withPositionVariables(
+    props: PropsWithPosition<{}>,
+    variableName: string
+) {
+    const styleWithPosition = {};
+
+    TWO_COORDINATION_PROPERTIES.map((property: TopAndLeft) => {
+        Object.assign(
+            styleWithPosition,
+            computePositionWithVariable(props?.position, variableName, property)
+        );
+    });
+
+    return styleWithPosition;
+}
+
+function computePositionWithVariable(
+    position: PropsWithPosition<{}>['position'],
+    variableName: string,
+    propertyName: Properties
+) {
+    if (position?.[propertyName] !== undefined) {
+        if (typeof position?.[propertyName] === 'number')
+            return { [`--${variableName}`]: `${position[propertyName]}px` };
+
+        return { [`--${variableName}`]: position[propertyName] };
+    }
+
+    return {};
+}
+
 type Properties = 'top' | 'bottom' | 'left' | 'right';
+type TopAndLeft = 'top' | 'left';
+type PositionTopAndLeft = Omit<Properties, 'right' | 'bottom'>;
