23fb21f68b4de016f6a63a02839d09031c109835
mikoloism <mikoloism@gmail.com>
2022-11-20 11:59:29 +0330

    perf(type): add `position` and `style` and `react reference` props types;

--- components/atoms/Door/types.ts
+++ components/atoms/Door/types.ts
@@ -1,8 +1,16 @@
-export type PropsWithPosition<T> = T & {
-    position?: {
-        top?: number | string;
-        left?: number | string;
-        right?: number | string;
-        bottom?: number | string;
-    };
+import { CSSProperties, RefObject } from 'react';
+
+export type PositionProperties = 'top' | 'right' | 'bottom' | 'left';
+
+export type PropsWithPosition<T = {}> = T & { position?: PositionType };
+
+export type PositionType = {
+    top?: number | string;
+    right?: number | string;
+    bottom?: number | string;
+    left?: number | string;
 };
+
+export type PropsWithStyle<T = {}> = T & { style?: CSSProperties };
+
+export type PropsWithRef<T = {}> = T & { reference: RefObject<HTMLDivElement> };
