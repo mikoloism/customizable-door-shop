7f2b986b1548d50ec40abdfae46054974b946d8c
mikoloism <mikoloism@gmail.com>
2022-11-22 21:17:14 +0330

    perf(page): update the styles and structure of elements in `door 3 box (from top to bottom direction)`;

--- pages/door-3.tsx
+++ pages/door-3.tsx
@@ -1,115 +1,365 @@
-import { useEffect, useLayoutEffect } from 'react';
 import { Group } from 'components/atoms/Door/Group';
 import { useBoundingClientRect } from 'components/atoms/Door/hooks';
 import { Panel } from 'components/atoms/Door/Panel';
 import { ProfileSeparator } from 'components/atoms/Door/ProfileSeparator';
 import { ProfileSide } from 'components/atoms/Door/ProfileSide';
-import { useRef } from 'react';
+import { KeyboardEvent, useState } from 'react';
 import style from 'styles/door-3-variables.module.scss';
 
 export default function HomePage() {
     const wrapper = useBoundingClientRect();
-    const group = useBoundingClientRect();
-    const top = useBoundingClientRect();
-    const bottom = useBoundingClientRect();
-    const left = useBoundingClientRect();
-    const right = useBoundingClientRect();
-    const innerGroup = useBoundingClientRect();
-    const panel1 = useBoundingClientRect();
-    const separator1 = useBoundingClientRect();
-    const panel2 = useBoundingClientRect();
-    const separator2 = useBoundingClientRect();
-    const panel3 = useBoundingClientRect();
+
+    const width = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            width.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--door-width',
+                    `${width.value}px`
+                );
+            }
+        },
+    };
+
+    const height = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            height.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--door-height',
+                    `${height.value}px`
+                );
+            }
+        },
+    };
+
+    const fixed1 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            fixed1.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-1-fixed-height',
+                    `${fixed1.value}px`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-1-relative-height',
+                    `0`
+                );
+                relative1.setValue(() => 0);
+            }
+        },
+    };
+
+    const relative1 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            relative1.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-1-relative-height',
+                    `${relative1.value}`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-1-fixed-height',
+                    `0px`
+                );
+                fixed1.setValue(() => 0);
+            }
+        },
+    };
+
+    const fixed2 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            fixed2.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-2-fixed-height',
+                    `${fixed2.value}px`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-2-relative-height',
+                    `0`
+                );
+                relative2.setValue(0);
+            }
+        },
+    };
+
+    const relative2 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            relative2.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-2-relative-height',
+                    `${relative2.value}`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-2-fixed-height',
+                    `0px`
+                );
+                fixed2.setValue(() => 0);
+            }
+        },
+    };
+
+    const fixed3 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            fixed3.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-3-fixed-height',
+                    `${fixed3.value}px`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-3-relative-height',
+                    `0`
+                );
+                relative3.setValue(0);
+            }
+        },
+    };
+
+    const relative3 = {
+        value: 0,
+        setValue: null as any,
+        onChange(event: React.ChangeEvent<HTMLInputElement>) {
+            relative3.setValue(() => event.target.value);
+        },
+        onKeyUp(event: KeyboardEvent<HTMLInputElement>) {
+            if (event.key === 'Enter') {
+                wrapper.current!.style.setProperty(
+                    '--panel-3-relative-height',
+                    `${relative3.value}`
+                );
+                wrapper.current!.style.setProperty(
+                    '--panel-3-fixed-height',
+                    `0px`
+                );
+                fixed3.setValue(() => 0);
+            }
+        },
+    };
+
+    [width.value, width.setValue] = useState<number>(500);
+    [height.value, height.setValue] = useState<number>(800);
+    [fixed1.value, fixed1.setValue] = useState<number>(0);
+    [relative1.value, relative1.setValue] = useState<number>(1);
+    [fixed2.value, fixed2.setValue] = useState<number>(0);
+    [relative2.value, relative2.setValue] = useState<number>(1);
+    [fixed3.value, fixed3.setValue] = useState<number>(0);
+    [relative3.value, relative3.setValue] = useState<number>(1);
 
     return (
-        <div
-            ref={wrapper.ref}
-            className={style['variables']}
-            style={{
-                width: '500px',
-                height: '800px',
-                position: 'relative',
-            }}>
-            <ProfileSide
-                reference={left.ref}
-                rotate="90"
-                position={{ left: 0 }}
-            />
+        <>
+            <div className="flex flex-col flex-wrap gap-y-9 fixed right-8 top-10 bg-blue-100 p-9 rounded-md">
+                <div className="flex flex-row flex-wrap w-full h-32 gap-x-3 bg-blue-200 p-9 rounded-md">
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>Width :</span>
+                        <input
+                            type="number"
+                            onKeyUp={width.onKeyUp}
+                            onChange={width.onChange}
+                            value={width.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>Height :</span>
+                        <input
+                            type="number"
+                            onKeyUp={height.onKeyUp}
+                            onChange={height.onChange}
+                            value={height.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                </div>
 
-            <Group
-                reference={group.ref}
-                position={{
-                    top: 0,
-                    bottom: 0,
-                    right: 'var(--profile-side-size)',
-                    left: 'var(--profile-side-size)',
+                <div className="flex flex-row flex-wrap w-full h-32 gap-x-3 bg-blue-200 p-9 rounded-md">
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(1) Fixed :</span>
+                        <input
+                            type="number"
+                            onKeyUp={fixed1.onKeyUp}
+                            onChange={fixed1.onChange}
+                            value={fixed1.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(1) Relative :</span>
+                        <input
+                            type="number"
+                            onKeyUp={relative1.onKeyUp}
+                            onChange={relative1.onChange}
+                            value={relative1.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                </div>
+
+                <div className="flex flex-row flex-wrap w-full h-32 gap-x-3 bg-blue-200 p-9 rounded-md">
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(2) Fixed :</span>
+                        <input
+                            type="number"
+                            onKeyUp={fixed2.onKeyUp}
+                            onChange={fixed2.onChange}
+                            value={fixed2.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(2) Relative :</span>
+                        <input
+                            type="number"
+                            onKeyUp={relative2.onKeyUp}
+                            onChange={relative2.onChange}
+                            value={relative2.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                </div>
+
+                <div className="flex flex-row flex-wrap w-full h-32 gap-x-3 bg-blue-200 p-9 rounded-md">
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(3) Fixed :</span>
+                        <input
+                            type="number"
+                            onKeyUp={fixed3.onKeyUp}
+                            onChange={fixed3.onChange}
+                            value={fixed3.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                    <label className="w-32 h-9 flex flex-col gap-y-2">
+                        <span>(3) Relative :</span>
+                        <input
+                            type="number"
+                            onKeyUp={relative3.onKeyUp}
+                            onChange={relative3.onChange}
+                            value={relative3.value}
+                            id="width"
+                            className="w-32 h-8 p-4 rounded-md bg-blue-100"
+                        />
+                    </label>
+                </div>
+            </div>
+
+            <div
+                ref={wrapper.ref}
+                className={style['variables']}
+                style={{
+                    position: 'relative',
+                    width: 'var(--door-width)',
+                    height: 'var(--door-height)',
                 }}>
                 <ProfileSide
-                    reference={top.ref}
-                    rotate="0"
-                    position={{ top: 0, right: 0, left: 0 }}
+                    rotate="90"
+                    position={{ left: 0 }}
+                    style={{ height: 'var(--door-height)' }}
                 />
 
                 <Group
-                    reference={innerGroup.ref}
                     position={{
-                        top: 'var(--profile-side-size)',
-                        bottom: 'var(--profile-side-size)',
-                        left: 0,
-                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        right: 'var(--profile-side-size)',
+                        left: 'var(--profile-side-size)',
                     }}>
-                    <Panel
-                        reference={panel1.ref}
-                        position={{ top: 'var(--panel-1-top)' }}
-                        style={{
-                            width: '100%',
-                            height: 'var(--panel-1-height)',
-                        }}
+                    <ProfileSide
+                        rotate="0"
+                        position={{ top: 0, right: 0, left: 0 }}
                     />
 
-                    <ProfileSeparator
-                        reference={separator1.ref}
-                        axis="horizontal"
-                        position={{ top: 'var(--separator-1-top)' }}
-                    />
+                    <Group
+                        position={{
+                            top: 'var(--profile-side-size)',
+                            bottom: 'var(--profile-side-size)',
+                            left: 0,
+                            right: 0,
+                        }}>
+                        <Panel
+                            position={{ top: 'var(--panel-1-top)' }}
+                            style={{
+                                height: 'var(--panel-1-height)',
+                                width: '100%',
+                            }}
+                        />
 
-                    <Panel
-                        reference={panel2.ref}
-                        position={{ top: 'var(--panel-2-top)' }}
-                        style={{
-                            height: 'var(--panel-2-height)',
-                            width: '100%',
-                        }}
-                    />
+                        <ProfileSeparator
+                            axis="horizontal"
+                            position={{ top: 'var(--separator-1-top)' }}
+                        />
 
-                    <ProfileSeparator
-                        reference={separator2.ref}
-                        axis="horizontal"
-                        position={{ top: 'var(--separator-2-top)' }}
-                    />
+                        <Panel
+                            position={{ top: 'var(--panel-2-top)' }}
+                            style={{
+                                height: 'var(--panel-2-height)',
+                                width: '100%',
+                            }}
+                        />
 
-                    <Panel
-                        reference={panel3.ref}
-                        position={{ top: 'var(--panel-3-top)' }}
-                        style={{
-                            height: 'var(--panel-3-height)',
-                            width: '100%',
-                        }}
+                        <ProfileSeparator
+                            axis="horizontal"
+                            position={{ top: 'var(--separator-2-top)' }}
+                        />
+
+                        <Panel
+                            position={{ top: 'var(--panel-3-top)' }}
+                            style={{
+                                height: 'var(--panel-3-height)',
+                                width: '100%',
+                            }}
+                        />
+                    </Group>
+
+                    <ProfileSide
+                        rotate="180"
+                        position={{ bottom: 0, right: 0, left: 0 }}
                     />
                 </Group>
 
                 <ProfileSide
-                    reference={bottom.ref}
-                    rotate="180"
-                    position={{ bottom: 0, right: 0, left: 0 }}
+                    rotate="270"
+                    position={{ right: 0 }}
+                    style={{ height: 'var(--door-height)' }}
                 />
-            </Group>
-
-            <ProfileSide
-                reference={right.ref}
-                rotate="270"
-                position={{ right: 0 }}
-            />
-        </div>
+            </div>
+        </>
     );
 }
